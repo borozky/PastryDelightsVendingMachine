876b1548b6b584e1fc3283da455dfbb0ad80f8f7
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-28 17:52:38 +1000

    Menu and main file implemented

--- vm.c
+++ vm.c
@@ -1,14 +1,69 @@
 /******************************************************************************
-** Student name: 	...
-** Student number: 	...
+** Student name: 	JOSHUA OROZCO
+** Student number: 	s3485376
 ** Course: 			Advanced Programming Techniques - S1 2018
 ******************************************************************************/
 
 #include "vm.h"
 
+#define DEFAULT_STOCK_FILE "stock.dat"
+#define DEFAULT_COIN_FILE "coins.dat"
+
 int main(int argc, char ** argv)
 {
-    printf("Goodbye. \n\n");
+    /**
+     * 1st argument is the executable and will always be given
+     * 2nd argument is the stock file. This should be optional.
+     * 3rd argument is the coin file. This should be optional.
+     */
+    char *stockFile;
+    char *coinFile;
+
+    printf("ARGC %d\n", argc);
+    switch (argc) {
+        /* both stock and coin file args are missing */
+        case 1:
+            stockFile = DEFAULT_STOCK_FILE;
+            coinFile = DEFAULT_COIN_FILE;
+            break;
+        /* stock file is given, coin file arg is missing */
+        case 2:
+            stockFile = argv[1];
+            coinFile = DEFAULT_COIN_FILE;
+            break;
+        /* both stock and coin files are supplied */
+        case 3:
+        default:
+            stockFile = argv[1];
+            coinFile = argv[2];
+            break;
+    }
+
+    
+    while (argv) {
+        printf("ARGV: %s", *argv);
+        argv++;
+    }
+
+    MenuFunction menuChoice;
+    MenuItem menuItem;
+    VmSystem vmSystem;
+
+    initMenu(&menuItem);
+    
+    do {
+        menuChoice = getMenuChoice(&menuItem);
+    }
+    while (menuChoice == NULL);
+
+    
+
+    
+
+    
+
+
+
 
     return EXIT_SUCCESS;
 }
--- vm_menu.c
+++ vm_menu.c
@@ -5,6 +5,7 @@
 ******************************************************************************/
 
 #include "vm_menu.h"
+#define NUM_OF_OPTIONS 9
 
 /**
  * vm_menu.c handles the initialisation and management of the menu array.
@@ -17,7 +18,68 @@
  * that will be called.
  **/
 void initMenu(MenuItem * menu)
-{ }
+{
+    int i = 0;
+    int j = 0;
+
+    /* menu will be NULL when passed. So initialize some memory */
+    menu = (MenuItem *) malloc(sizeof(MenuItem) * NUM_OF_OPTIONS);
+    if (menu == NULL) {
+        printf("CANNOT INITIALIZE MENU ITEMS");
+        exit(0);
+    }
+
+    /* array of strings of menu items */
+    char* menuItemTexts[NUM_OF_OPTIONS] = {
+        "Display items",
+        "Purchase items",
+        "Save and Exit",
+        "Add item",
+        "Remove item",
+        "Display coins",
+        "Reset stock",
+        "Reset coins",
+        "Abort Program"
+    };
+
+    /* array of function pointers */
+    MenuFunction menuFunctions[NUM_OF_OPTIONS] = {
+        displayItems,
+        purchaseItem,
+        saveAndExit,
+        addItem,
+        removeItem,
+        displayCoins,
+        resetStock,
+        resetCoins,
+        abortProgram
+    };
+
+
+    /* assign text and callbacks */
+    for (i = 0; i < NUM_OF_OPTIONS; i++) {
+        /**
+         * Using this code below produce error 'array type 'char [51]' is not assignable'
+         * menu[i].text = menuItemText[i]
+         * So copy the strings instead of referencing them
+         */
+        strcpy(menu[i].text, menuItemTexts[i]);
+        menu[i].function = menuFunctions[i];
+    }
+
+
+    for (j = 0; j < NUM_OF_OPTIONS; j++) {
+        if (j == 0) {
+            printf("Main Menu:\n");
+        }
+        else if (j == 3) {
+            printf("Administrator-Only Menu:\n");
+        }
+        printf("%d. %s\n", j + 1, menuItemTexts[j]);
+    }
+
+
+}
 
 /**
  * Gets input from the user and returns a pointer to the MenuFunction
@@ -26,5 +88,32 @@ void initMenu(MenuItem * menu)
  **/
 MenuFunction getMenuChoice(MenuItem * menu)
 {
+    int buffer;
+    char* enteredInput;
+    int inputNumber;
+    char *endPointer; /* all non string inputs will be collected here */
+    MenuFunction function;
+
+    inputNumber = 0;
+    buffer = sizeof(int);
+    enteredInput = (char *) malloc(buffer);
+
+    printf("Select your options (1-9): ");
+    enteredInput = fgets(enteredInput, buffer, stdin);
+    fflush(stdin);
+    printf("Entered: %s\n", enteredInput);
+
+    /**
+     * endPointer must not be NULL so don't initialize 
+     * endPointer as '**endPointer' becasue it will be 
+     * NULL to begin with */
+    inputNumber = strtol(enteredInput, &endPointer, 10);
+    printf("Detected number: %d\n", inputNumber);
+
+    if (inputNumber > 0 && inputNumber <= NUM_OF_OPTIONS) {
+        return *(menu + (inputNumber - 1))->function; 
+    }
+
     return NULL;
+
 }
