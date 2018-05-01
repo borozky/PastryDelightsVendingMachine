c51e7fdb426fc8f806ed4afe12ce139a50591997
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-01 22:28:52 +1000

    basic impl of vm options

--- vm_options.c
+++ vm_options.c
@@ -6,6 +6,11 @@
 
 #include "vm_options.h"
 
+#define DEFAULT_STOCK_FILE "stock.dat"
+#define DEFAULT_COIN_FILE "coins.dat"
+#define STOCK_LINE_SIZE 1024
+#define COIN_LINE_SIZE 64
+
 /**
  * vm_options.c this is where you need to implement the system handling
  * functions (e.g., init, free, load, save) and the main options for
@@ -19,7 +24,17 @@
  **/
 Boolean systemInit(VmSystem * system)
 {
-    return FALSE;
+    if (system == NULL) {
+        system = (VmSystem *) malloc(sizeof(*system));
+        (*system).coinFileName = DEFAULT_COIN_FILE;
+        (*system).stockFileName = DEFAULT_STOCK_FILE;
+    }
+
+    if (loadData(system, (*system).stockFileName, (*system).coinFileName) == FALSE) {
+        return FALSE;
+    }
+
+    return TRUE;
 }
 
 /**
@@ -28,7 +43,9 @@ Boolean systemInit(VmSystem * system)
  * and run it through valgrind.
  **/
 void systemFree(VmSystem * system)
-{ }
+{ 
+    return;
+}
 
 /**
  * Loads the stock and coin data into the system. You will also need to assign
@@ -39,6 +56,12 @@ void systemFree(VmSystem * system)
 Boolean loadData(
     VmSystem * system, const char * stockFileName, const char * coinsFileName)
 {
+    if (loadStock(system, stockFileName) && loadCoins(system, coinsFileName)) {
+        return TRUE;
+    }
+
+    /* TODO: clean up here if above condition is false */
+
     return FALSE;
 }
 
@@ -47,7 +70,34 @@ Boolean loadData(
  **/
 Boolean loadStock(VmSystem * system, const char * fileName)
 {
-    return FALSE;
+    FILE *stock_file;
+    char *line;
+    Stock *stock;
+    
+    /* open stock file in read-mode, return FALSE if not found */
+    stock_file = fopen(fileName, "r");
+    if (stock_file == NULL) {
+        fprintf(stderr, "Stock file '%s' doesn't exists or unreadable", fileName);
+        return FALSE;
+    }
+
+    /* creates a linked list (see vm_stock.c) */
+    (*system).itemList = create_list();
+
+    /* gets each lines */
+    line = (char *) malloc(sizeof(*line) * STOCK_LINE_SIZE);
+    while (TRUE) {
+        fgets(line, STOCK_LINE_SIZE, stock_file);
+        if (feof(stock_file)) {
+            break;
+        }
+
+        /* create and insert the stock into VmSystem's itemList */
+        stock = create_stock(line);
+        add_stock_item((*system).itemList, stock);
+    }
+
+    return TRUE;
 }
 
 /**
@@ -55,7 +105,35 @@ Boolean loadStock(VmSystem * system, const char * fileName)
  **/
 Boolean loadCoins(VmSystem * system, const char * fileName)
 {
-    return FALSE;
+    FILE *coin_file;
+    char *line;
+    Coin *coin;
+    int line_number;
+
+    /* open the coin file, or return false is not found */
+    coin_file = fopen(fileName, "r");
+    if (coin_file == NULL) {
+        fprintf(stderr, "Coin file '%s' doesn't exists or unreadable", fileName);
+        return FALSE;
+    }
+
+    /* read each line */
+    line_number = 0;
+    line = (char *) malloc(sizeof(*line) * COIN_LINE_SIZE);
+    while (TRUE) {
+        fgets(line, COIN_LINE_SIZE, coin_file);
+        if (feof(coin_file)) {
+            break;
+        }
+
+        /* add new coin */
+        coin = create_coin(line);
+        (*system).cashRegister[line_number] = *coin;
+        line_number += 1;
+    }
+
+
+    return TRUE;
 }
 
 /**
@@ -79,14 +157,45 @@ Boolean saveCoins(VmSystem * system)
  * This is the data loaded into the linked list in the requirement 2.
  **/
 void displayItems(VmSystem * system)
-{ }
+{ 
+    Node *node;
+    Stock *stock;
+
+    node = (*system).itemList->head;
+    printf("Items Menu\n");
+
+    if (node == NULL) {
+        printf("Sorry, there are no items in the list\n");
+        return;
+    }
+
+    printf("%5s | %30s | %10s | %7s \n", "ID", "Name", "Available", "Price");
+    printf("---------------------------------------------------\n");
+
+    while (node != NULL) {
+        stock = (*node).data;
+        printf("%5s | %30s | %10d | $%-2d.%5d \n",
+            (*stock).id,
+            (*stock).name,
+            (*stock).onHand,
+            (*stock).price.dollars,
+            (*stock).price.cents
+        );
+        node = (*node).next;
+    }
+    printf("DISPLAY ITEMS\n");
+    return;
+}
 
 /**
  * This option allows the user to purchase an item.
  * This function implements requirement 5 of the assignment specification.
  **/
 void purchaseItem(VmSystem * system)
-{ }
+{ 
+    printf("PURCHASE ITEMS\n");
+    return;
+}
 
 /**
  * You must save all data to the data files that were provided on the command
@@ -94,21 +203,30 @@ void purchaseItem(VmSystem * system)
  * This function implements requirement 6 of the assignment specification.
  **/
 void saveAndExit(VmSystem * system)
-{ }
+{ 
+    printf("SAVE AND EXIT\n");
+    return;
+}
 
 /**
  * This option adds an item to the system. This function implements
  * requirement 7 of of assignment specification.
  **/
 void addItem(VmSystem * system)
-{ }
+{ 
+    printf("ADD ITEM\n");
+    return;
+}
 
 /**
  * Remove an item from the system, including free'ing its memory.
  * This function implements requirement 8 of the assignment specification.
  **/
 void removeItem(VmSystem * system)
-{ }
+{ 
+    printf("REMOVE ITEM\n");
+    return;
+}
 
 /**
  * This option will require you to display the coins from lowest to highest
@@ -117,7 +235,10 @@ void removeItem(VmSystem * system)
  * specifications.
  **/
 void displayCoins(VmSystem * system)
-{ }
+{ 
+    printf("DISPLAY COINS\n");
+    return;
+}
 
 /**
  * This option will require you to iterate over every stock in the
@@ -126,7 +247,10 @@ void displayCoins(VmSystem * system)
  * This function implements requirement 9 of the assignment specification.
  **/
 void resetStock(VmSystem * system)
-{ }
+{ 
+    printf("RESET STOCK\n");
+    return;
+}
 
 /**
  * This option will require you to iterate over every coin in the coin
@@ -136,11 +260,17 @@ void resetStock(VmSystem * system)
  * assignment specifications.
  **/
 void resetCoins(VmSystem * system)
-{ }
+{ 
+    printf("RESET COINS\n");
+    return;
+}
 
 /**
  * This option will require you to display goodbye and free the system.
  * This function implements requirement 10 of the assignment specification.
  **/
 void abortProgram(VmSystem * system)
-{ }
+{ 
+    printf("ABORT PROGRAM\n");
+    return;
+}
